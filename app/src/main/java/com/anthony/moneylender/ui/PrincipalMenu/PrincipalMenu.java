4b003849eecd19d2a64edb951a72bf643f35e26f
anthonyR012 <rubionn27@gmail.com>
2021-08-05 20:11:46 -0500

    Feat: Creation new menu for users registrador

--- app/src/main/java/com/anthony/moneylender/ui/PrincipalMenu/PrincipalMenu.java
+++ app/src/main/java/com/anthony/moneylender/ui/PrincipalMenu/PrincipalMenu.java
@@ -3,6 +3,9 @@ package com.anthony.moneylender.ui.PrincipalMenu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.GridLayout;
+import android.widget.TextView;
 
 import com.anthony.moneylender.R;
 import com.anthony.moneylender.ui.PrincipalMenu.Fragments.InicioFragment;
@@ -13,5 +16,7 @@ public class PrincipalMenu extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal_menu);
+
+
     }
 }
